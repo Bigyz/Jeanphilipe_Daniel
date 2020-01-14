027cbf8be1d94608668a0011b761bc1da0873c08
Bigyz <adaniel.da41@gmail.com>
2020-01-14 17:22:23 +0100

    exo_07_c.rb

---
+++ exo_07_c.rb
@@ -0,0 +1,16 @@
+puts "Nom"
+print "> "
+user_name = gets.chomp
+puts user_name
+puts "Prenom"
+print "> "
+user_name = gets.chomp
+puts user_name
+puts "Age"
+print "> "
+user_name = gets.chomp
+puts user_name
+#Dans le premier le '>' n'est pas présent
+#Dans le deuxieme il l'est
+#Dans le troisieme j'ai rajouté 2 chomp
+
