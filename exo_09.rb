bb1c36e50c9da3a55b9d56f92559c55ae6b63466
Bigyz <adaniel.da41@gmail.com>
2020-01-14 17:17:04 +0100

    exo_09.rb

---
+++ exo_09.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, c'est quoi ton Prenom ?"
+print "> "
+user_name = gets.chomp
+puts "Et ton Nom ?"
+print "> "
+user_lastname = gets.chomp
+puts "Bonjour, " + user_name + " " + user_lastname + " !"
